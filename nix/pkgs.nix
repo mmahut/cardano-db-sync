b4108ba927db7dd743b1974e807d8dc04c6111a0
Craige McWhirter <craige.mcwhirter@iohk.io>
2020-10-08 14:14:32 +1000

    [SRE-141] Add hlint ci check

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,21 +1,22 @@
 # our packages overlay
-pkgs: _: with pkgs; {
-  cardanoDbSyncHaskellPackages = callPackage ./haskell.nix {};
-
-  cardanoNodeHaskellPackages = import ./haskell.nix {
-    inherit compiler
-      pkgs
-      lib
-      stdenv
-      haskell-nix
-      buildPackages
-      gitrev
-      ;
-  };
+pkgs: _: with pkgs;
+  let
+    compiler = config.haskellNix.compiler or "ghc865";
+  in {
+    cardanoDbSyncHaskellPackages = callPackage ./haskell.nix {
+      inherit compiler;
+    };
 
   # Grab the executable component of our package.
   inherit (cardanoDbSyncHaskellPackages.cardano-db-sync.components.exes)
       cardano-db-sync;
   inherit (cardanoDbSyncHaskellPackages.cardano-db-sync-extended.components.exes)
       cardano-db-sync-extended;
+
+  inherit ((haskell-nix.hackage-package {
+    name = "hlint";
+    version = "3.1.6";
+    compiler-nix-name = compiler;
+    inherit (cardanoDbSyncHaskellPackages) index-state;
+  }).components.exes) hlint;
 }
